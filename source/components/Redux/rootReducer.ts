c933df3092d24fe6037f8f2e83d50ee6c2b61a1d
Artyom <akuzikov@mail.ru>
2020-08-04 11:23:20 +0300

    added redux base

---
+++ source/components/Redux/rootReducer.ts
@@ -0,0 +1,3 @@
+import { ADD_ITEM, REMOVE_ITEM, REMOVE_ALL, TOGGLE_STATUS, TOGGLE_ALL, SHOW_ALL, SHOW_ACTIVE, SHOW_COMPLETED  } from './types';
+import { combineReducers } from "redux";
+
---
+++ source/components/Redux/types.ts
@@ -0,0 +1,8 @@
+export const ADD_ITEM: string = 'ADD_ITEM';
+export const REMOVE_ITEM: string = 'REMOVE_ITEM';
+export const TOGGLE_STATUS: string = 'TOGGLE_STATUS';
+export const TOGGLE_ALL: string = 'TOGGLE_ALL';
+export const REMOVE_ALL: string = 'REMOVE_ALL';
+export const SHOW_ALL: string = 'SHOW_ALL';
+export const SHOW_ACTIVE: string = 'SHOW_ACTIVE';
+export const SHOW_COMPLETED: string = 'SHOW_COMPLETED';
